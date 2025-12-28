c24d5af0307bbc15052f746f2f1d844a8bcd1c05
DCDS Karunanayaka <kdchamiladesilva@gmail.com>
2025-12-28 17:58:56 +0530

    feat: update Clerk dependencies and implement user creation on login

--- app/provider.tsx
+++ app/provider.tsx
@@ -1,5 +1,9 @@
-import React from 'react'
+"use client"
+import React, { use, useEffect } from 'react'
 import Header from './_components/Header';
+import { useMutation } from 'convex/react';
+import { api } from '@/convex/_generated/api';
+import { useUser } from '@clerk/nextjs';
 
 function Provider({
   children,
@@ -7,10 +11,26 @@ function Provider({
   children: React.ReactNode;
 }>) {
 
+  const CreateUser=useMutation(api.user.CreateNewUser)
 
-  const CreateNewUser = () => {
+  const {user}=useUser(); // Custom Hook to get User Info
+
+  useEffect(() => {
+    user&& CreateNewUser();
+  }, [user])
+
+  const CreateNewUser = async() => {
+    if(user)
+    {
      // Save New User if Not Exist
+     const result=await CreateUser({
+      email:user?.primaryEmailAddress?.emailAddress??'',
+      imageUrl:user?.imageUrl,
+      name:user?.fullName??'',
+
+     })
   }
+}
 
   return (
     <div>
