c6be65be8ffca111b3a4c7c13e0976fa88b77776
DCDS Karunanayaka <kdchamiladesilva@gmail.com>
2025-12-28 18:32:46 +0530

    feat: implement UserDetailContext and update Provider to manage user details

--- app/provider.tsx
+++ app/provider.tsx
@@ -1,9 +1,11 @@
 "use client"
-import React, { use, useEffect } from 'react'
+import React, { use, useContext, useEffect, useState } from 'react'
 import Header from './_components/Header';
 import { useMutation } from 'convex/react';
 import { api } from '@/convex/_generated/api';
 import { useUser } from '@clerk/nextjs';
+import { UserDetailContext } from '@/context/UserDetailContext';
+import { User } from 'lucide-react';
 
 function Provider({
   children,
@@ -12,7 +14,7 @@ function Provider({
 }>) {
 
   const CreateUser=useMutation(api.user.CreateNewUser)
-
+  const [userDetail,setUserDetail]=useState<any>()
   const {user}=useUser(); // Custom Hook to get User Info
 
   useEffect(() => {
@@ -28,16 +30,23 @@ function Provider({
       imageUrl:user?.imageUrl,
       name:user?.fullName??'',
 
-     })
+     });
+     setUserDetail(result);
   }
 }
 
   return (
+    <UserDetailContext.Provider value={{userDetail,setUserDetail}}>
     <div>
       <Header />  
       {children}
     </div>
+    </UserDetailContext.Provider>
   )
 }
 
 export default Provider
+
+export const useUserDetail=()=>{
+  return useContext (UserDetailContext);
+}
